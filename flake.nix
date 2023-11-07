edea82231183156fb2dc85fd0ebc7ca320cace17
Max Verbeek <m4xv3rb33k@gmail.com>
2023-11-07 12:00:23 +0100

    fix: add fyne-cross to nix

--- flake.nix
+++ flake.nix
@@ -6,11 +6,25 @@
 
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        fyne-cross = pkgs.buildGoModule {
+          name = "fyne-cross";
+          doCheck = false;
+          src = pkgs.fetchFromGitHub {
+            owner = "fyne-io";
+            repo = "fyne-cross";
+            rev = "f8b06cf33396ad174fa7169216560a9d63e6cd73";
+            hash = "sha256-dNd6D85GRgjHHS1D7IVvsznC8hn8sREQU71RhshZLbY=";
+          };
+          vendorHash = "sha256-68WD7kij+au0cwnnbB1U1OA3Bb8vjkdPbhV6aiQcjgo=";
+        };
+
       in {
         devShell = pkgs.mkShell {
           name = "devshell";
           buildInputs = with pkgs; [
+            fyne-cross
             libGL
             pkg-config
             xorg.libX11.dev
