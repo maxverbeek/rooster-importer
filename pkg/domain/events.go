9bf0d9011b83f558202be6edd74ac60d2148cc59
Max Verbeek <m4xv3rb33k@gmail.com>
2025-04-13 15:07:21 +0200

    feat: update rooster tijden

--- pkg/domain/events.go
+++ pkg/domain/events.go
@@ -35,110 +35,180 @@ func dateToTime(date time.Time) time.Time {
 }
 
 func NewScheduleEvent(excelEntry string, date time.Time) (*ScheduleEvent, Conversion) {
-	if strings.HasPrefix(excelEntry, "i") {
-		// Try removing the i (could mean inwerken) prefix
-		noprefix := excelEntry[1:]
-		inwerkEvent, inwerkConversion := NewScheduleEvent(noprefix, date)
-
-		// If the result turns into "vrij" (omitted from calendar) or a default, ignore it
-		// and try again with the full text
-		if inwerkConversion != ConversionDefaulted && inwerkConversion != ConversionVrij {
-			inwerkEvent.ScheduleType = excelEntry
-
-			return inwerkEvent, inwerkConversion
-		}
-	}
+	weekday := timeAtDay(date, 16, 0).Weekday()
+	isWeekend := weekday == time.Saturday || weekday == time.Sunday
 
 	switch strings.ToLower(strings.Trim(excelEntry, " ")) {
 	case "":
-		// weekend dagen niet meenemen
-		return nil, ConversionSkipped
-	case "aanvraag verlof":
 		fallthrough
-	case "aanvraag vrij":
+	case "x":
 		fallthrough
-	case "vak":
+	case "-":
 		fallthrough
-	case "vk":
-		// Vakantie dag
+	case "-c":
+		conversion := ConversionConverted
+		if isWeekend {
+			conversion = ConversionSkipped
+		}
+
 		event := ScheduleEvent{
-			ScheduleType: "Vakantiedag",
+			ScheduleType: "Vrij",
 			Start:        dateToTime(date),
 			End:          dateToTime(date.Add(24 * time.Hour)),
 			AllDay:       true,
 		}
+		return &event, conversion
 
-		return &event, ConversionVrij
-	case "c":
-		// Vrij/Compensatie
+	case "d":
+		fallthrough
+	case "d (als)":
 		event := ScheduleEvent{
-			ScheduleType: "Compensatiedag",
-			Start:        dateToTime(date),
-			End:          dateToTime(date.Add(24 * time.Hour)),
-			AllDay:       true,
+			ScheduleType: "Dag",
+			Start:        timeAtDay(date, 7, 45),
+			End:          timeAtDay(date, 16, 15),
+			AllDay:       false,
 		}
+		return &event, ConversionConverted
 
-		return &event, ConversionVrij
+	case "t":
+		fallthrough
+	case "t (als)":
+		event := ScheduleEvent{
+			ScheduleType: "Tussen",
+			Start:        timeAtDay(date, 11, 0),
+			End:          timeAtDay(date, 19, 30),
+			AllDay:       false,
+		}
+		return &event, ConversionConverted
 
 	case "a":
-		fallthrough
-	case "wa":
-		// (weekend) avond dienst
 		event := ScheduleEvent{
 			ScheduleType: "Avond",
-			Start:        timeAtDay(date, 16, 0),
-			End:          timeAtDay(date, 23, 59),
+			Start:        timeAtDay(date, 15, 0),
+			End:          timeAtDay(date, 23, 30),
+			AllDay:       false,
 		}
-
 		return &event, ConversionConverted
 
 	case "n":
-		fallthrough
-	case "wn":
-		// (weekend) nachtdienst
 		event := ScheduleEvent{
 			ScheduleType: "Nacht",
-			Start:        timeAtDay(date, 23, 30),
-			End:          timeAtDay(date.Add(time.Hour*24), 8, 30),
+			Start:        timeAtDay(date, 23, 0),
+			End:          timeAtDay(date.Add(24*time.Hour), 8, 30),
+			AllDay:       false,
 		}
-
 		return &event, ConversionConverted
 
-	case "wk":
-		// weekend kort
+	case "vak":
+		fallthrough
+	case "vak.":
 		event := ScheduleEvent{
-			ScheduleType: "Weekend kort",
-			Start:        timeAtDay(date, 8, 0),
-			End:          timeAtDay(date, 13, 0),
+			ScheduleType: "Vakantie",
+			Start:        dateToTime(date),
+			End:          dateToTime(date.Add(24 * time.Hour)),
+			AllDay:       true,
 		}
-
 		return &event, ConversionConverted
 
-	case "1b":
-		fallthrough
-	case "1d":
-		fallthrough
-	case "ccu":
-		fallthrough
-	case "seh":
-		fallthrough
-	case "seh l":
-		fallthrough
-	case "seh c":
-		// Normale dienst
-		event := ScheduleEvent{
-			ScheduleType: excelEntry,
-			Start:        timeAtDay(date, 8, 0),
-			End:          timeAtDay(date, 17, 0),
-		}
-
-		return &event, ConversionConverted
 	}
 
-	// Fall back to 8 - 17 but with a warning
+	// default naar dagdienst met een waarschuwing als het roostertype niet herkent wordt.
 	return &ScheduleEvent{
-		ScheduleType: excelEntry,
-		Start:        timeAtDay(date, 8, 0),
-		End:          timeAtDay(date, 17, 0),
+		ScheduleType: "Dag",
+		Start:        timeAtDay(date, 7, 45),
+		End:          timeAtDay(date, 16, 15),
+		AllDay:       false,
 	}, ConversionDefaulted
+
+	// case "":
+	// 	// weekend dagen niet meenemen
+	// 	return nil, ConversionSkipped
+	// case "aanvraag verlof":
+	// 	fallthrough
+	// case "aanvraag vrij":
+	// 	fallthrough
+	// case "vak":
+	// 	fallthrough
+	// case "vk":
+	// 	// Vakantie dag
+	// 	event := ScheduleEvent{
+	// 		ScheduleType: "Vakantiedag",
+	// 		Start:        dateToTime(date),
+	// 		End:          dateToTime(date.Add(24 * time.Hour)),
+	// 		AllDay:       true,
+	// 	}
+	//
+	// 	return &event, ConversionVrij
+	// case "c":
+	// 	// Vrij/Compensatie
+	// 	event := ScheduleEvent{
+	// 		ScheduleType: "Compensatiedag",
+	// 		Start:        dateToTime(date),
+	// 		End:          dateToTime(date.Add(24 * time.Hour)),
+	// 		AllDay:       true,
+	// 	}
+	//
+	// 	return &event, ConversionVrij
+	//
+	// case "a":
+	// 	fallthrough
+	// case "wa":
+	// 	// (weekend) avond dienst
+	// 	event := ScheduleEvent{
+	// 		ScheduleType: "Avond",
+	// 		Start:        timeAtDay(date, 16, 0),
+	// 		End:          timeAtDay(date, 23, 59),
+	// 	}
+	//
+	// 	return &event, ConversionConverted
+	//
+	// case "n":
+	// 	fallthrough
+	// case "wn":
+	// 	// (weekend) nachtdienst
+	// 	event := ScheduleEvent{
+	// 		ScheduleType: "Nacht",
+	// 		Start:        timeAtDay(date, 23, 30),
+	// 		End:          timeAtDay(date.Add(time.Hour*24), 8, 30),
+	// 	}
+	//
+	// 	return &event, ConversionConverted
+	//
+	// case "wk":
+	// 	// weekend kort
+	// 	event := ScheduleEvent{
+	// 		ScheduleType: "Weekend kort",
+	// 		Start:        timeAtDay(date, 8, 0),
+	// 		End:          timeAtDay(date, 13, 0),
+	// 	}
+	//
+	// 	return &event, ConversionConverted
+	//
+	// case "1b":
+	// 	fallthrough
+	// case "1d":
+	// 	fallthrough
+	// case "ccu":
+	// 	fallthrough
+	// case "seh":
+	// 	fallthrough
+	// case "seh l":
+	// 	fallthrough
+	// case "seh c":
+	// 	// Normale dienst
+	// 	event := ScheduleEvent{
+	// 		ScheduleType: excelEntry,
+	// 		Start:        timeAtDay(date, 8, 0),
+	// 		End:          timeAtDay(date, 17, 0),
+	// 	}
+	//
+	// 	return &event, ConversionConverted
+	// }
+	//
+	// // Fall back to 8 - 17 but with a warning
+	// return &ScheduleEvent{
+	// 	ScheduleType: excelEntry,
+	// 	Start:        timeAtDay(date, 8, 0),
+	// 	End:          timeAtDay(date, 17, 0),
+	// }, ConversionDefaulted
 }
